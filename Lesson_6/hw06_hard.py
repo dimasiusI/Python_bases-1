b7679946449cdfd0cb7c73878e129a52ff71306c
dimasiusI <dimasius93@gmail.com>
2019-02-13 19:09:54 +0300

    Решено

--- Lesson_6/hw06_hard.py
+++ Lesson_6/hw06_hard.py
@@ -13,3 +13,38 @@
 каждый работник получал строку из файла
 
 '''
+with open('data/workers', 'r', encoding='utf-8') as f:
+	w_list = f.readlines()
+
+with open('data/hours_of', 'r', encoding='utf-8') as f:
+	h_list = f.readlines()
+
+def parse(x):
+	name, surname, hours = x.split()
+	return name + ' ' + surname, float(hours)
+
+class Worker:
+	def __init__(self, s):
+		name, surname, salary, position, norm = s.split()
+		self.fullname = name + ' ' + surname
+		self.salary = float(salary)
+		self.position = position
+		self.norm = float(norm)
+
+	def calc_zp(self):
+		hours = work_hours.get(self.fullname, None)
+		if hours is None:
+			return None
+		if hours >= self.norm:
+			real_salary = self.salary * (1 + 2 * (hours / self.norm - 1))
+		else:
+			real_salary = self.salary * (hours / self.norm)
+		return round(real_salary, 2)
+
+
+workers = list(map(Worker, w_list[1:]))
+work_hours = {key: value for key, value in map(parse, h_list[1:])}
+
+real_zp = {worker.fullname: worker.calc_zp() for worker in workers}
+
+print(real_zp)
--- Lesson_6/hw06_normal.py
+++ Lesson_6/hw06_normal.py
@@ -2,3 +2,39 @@
 Задача-1: Написать класс для фигуры-треугольника, заданного координатами трех точек.
 Определить методы, позволяющие вычислить: площадь, высоту и периметр фигуры.
 '''
+
+import math
+
+class A:
+    def __init__(self, x1, y1, x2, y2, x3, y3):
+        self.x1 = math.fabs(x1)
+        self.y1 = math.fabs(y1)
+        self.x2 = math.fabs(x2)
+        self.y2 = math.fabs(y2)
+        self.x3 = math.fabs(x3)
+        self.y3 = math.fabs(y3)
+        self.ab = round(math.sqrt(math.pow((self.x2-self.x1), 2)+math.pow((self.y2-self.y1), 2)))
+        self.bc = round(math.sqrt(math.pow((self.x3-self.x2), 2)+math.pow((self.y3-self.y2), 2)))
+        self.ca = round(math.sqrt(math.pow((self.x3-self.x1), 2)+math.pow((self.y3-self.y1), 2)))
+        self.p = self.per / 2
+
+    @property
+    def per(self):
+        return self.ab + self.bc + self.ca
+
+    @property
+    def square(self):
+        return round((self.ca/2)*self.heght_ca)
+
+
+    @property
+    def heght_ca (self):
+
+        return round(2*(math.sqrt(self.p*(self.p-self.ab)*(self.p-self.bc)*(self.p-self.ca)))/self.ca)
+
+
+treugolnik = A(0, 0, 5, 12, 10, 0)
+
+print('Площадь: {}'.format(treugolnik.square))
+print('Высота: {}'.format(treugolnik.heght_ca))
+print('Периметр: {}'.format(treugolnik.per))
