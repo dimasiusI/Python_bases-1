7d9ff677d1c7b0aeb745a40aedb9534f160f3ab8
dimasiusI <dimasius93@gmail.com>
2019-02-15 16:31:32 +0300

    Решено

--- Lesson_7/hw07_normal.py
+++ Lesson_7/hw07_normal.py
@@ -6,3 +6,33 @@
 Метод __add__ должен реализовывать сложение матриц, а __str__ - вывод итоговой
 матрицы.
 '''
+
+class Matrix:
+    def __init__ (self, list1):
+        self.list1 = list1
+        self.mis = self.get_str()
+
+    def get_str (self):
+        r = ''
+        for i in self.list1:
+            r += str(i).replace(',', ' ').replace('[', '|').replace(']', '|') + '\n'
+        return r
+
+    def __add__ (self, other):
+        res = []
+        for i in range(len(self.list1)):
+            row = []
+            for j in range(len(self.list1[0])):
+                row.append(self.list1[i][j] + other.list1[i][j])
+            res.append(row)
+        return Matrix(res)
+
+    def __str__ (self):
+        return self.mis
+
+
+matrix1 = Matrix([[1,2,3,5], [1,2,3,6], [1,2,3,7]])
+matrix2 = Matrix([[1,2,3,5], [1,2,3,6], [1,2,3,7]])
+
+print('Результат сложения матриц: ')
+print(matrix1+matrix2)
